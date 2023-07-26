050f273440baa9e973c61cd765a560ea0ad877d8
Nathan Doucette <fluffyandnathan@gmail.com>
2023-07-26 00:22:16 -0400

    solved problem 3

--- 3/3.rs
+++ 3/3.rs
@@ -1,1 +1,19 @@
+fn main(){
+    let bignum:i64 = 600851475143;
+    for i in 2..((bignum as f64).sqrt()) as i32{
+        if(bignum % i as i64 == 0){
+            if(isPrime(i)){
+                println!("{:?}", i);
+            }
+        }
+    }
+}
+fn isPrime(num:i32) -> bool{
+    for i in 2..((num as f64).sqrt()) as i32{
+        if num % i == 0{
+            return false;
+        }
+    }
+    return true;
+}
 
